c36bbadd48a17aa5b4585a28e060b1945e5973d9
Alain O'Dea <alain.odea@gmail.com>
2011-09-16 22:26:12 -0230

    DISQUS Comment Service Support

---
+++ README.markdown
@@ -0,0 +1,6 @@
+DISQUS Comment Service integration for Zotonic
+==============================================
+Usage
+-----
+Make sure **DISQUS** is enabled in **Modules**
+
---
+++ mod_comment_disqus.erl
@@ -0,0 +1,15 @@
+%% @author Alain O'Dea <alain.odea@gmail.com>
+%% @copyright 2011 Alain O'Dea
+%% @date 2011-09-16
+%% @doc The DISQUS Comment module, implementing integration with the DISQUS comment service
+%%
+%% @license
+%% Apache 2.0 license.
+%%
+
+-module(mod_comment_disqus).
+-author("Alain O'Dea <alain.odea@gmail.com>").
+
+-mod_title("DISQUS").
+-mod_description("Integration with the DISQUS comment service").
+
